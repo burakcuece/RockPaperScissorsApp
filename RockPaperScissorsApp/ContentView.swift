e247d7ce09b32d12da682c6d71cfbc63bf71d57b
Burak Cüce <b.cuece@web.de>
2022-07-19 14:40:53 +0200

    Initial Commit

---
+++ RockPaperScissorsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissorsApp
+//
+//  Created by Burak Cüce on 19.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissorsApp/RockPaperScissorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsAppApp.swift
+//  RockPaperScissorsApp
+//
+//  Created by Burak Cüce on 19.07.22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
