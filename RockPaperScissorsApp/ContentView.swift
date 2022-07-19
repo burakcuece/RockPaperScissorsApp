04daebe9faae6e561db510e636a4392a3aa31fac
Burak Cüce <b.cuece@web.de>
2022-07-19 15:11:08 +0200

    - edit ContentView

--- RockPaperScissorsApp/ContentView.swift
+++ RockPaperScissorsApp/ContentView.swift
@@ -12,29 +12,51 @@ struct ContentView: View {
     @State var cpuPoints = 0
     @State var playerPoints = 0
     
+    @State var cpuImage = "rock"
+    @State var playerImage = "rock"
+    
     var body: some View {
         VStack {
             
-            Text("Points: 0")
+            Text("Points: \(cpuPoints)")
                 .font(.system(size: 24))
                 .bold()
             
-            Image("rock")
+            Image(cpuImage)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 200, height: 200)
             
-            Image("rock")
+            Image(playerImage)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 200, height: 200)
+                .onTapGesture {
+                    let cpuTap = Int.random(in: 1...3)
+                    cpuImage = imageGenerator(cpuTap)
+                    
+                    let myTap = Int.random(in: 1...3)
+                    playerImage = imageGenerator(myTap)
+                }
             
-            Text("Points: 0")
+            Text("Points: \(playerPoints)")
                 .font(.system(size: 24))
                 .bold()
             
         }
     }
+    
+    func imageGenerator(_ num: Int) -> String {
+        if num == 1 {
+            return "rock"
+        } else if num == 2 {
+            return "paper"
+        } else if num == 3 {
+            return "scissors"
+        }
+        return ""
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
