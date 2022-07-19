021915453028c1ffe1b08c57870ee08d9f3be2b4
Burak Cüce <b.cuece@web.de>
2022-07-19 15:03:57 +0200

    - add Images to Assets - edit ContentView

--- RockPaperScissorsApp/ContentView.swift
+++ RockPaperScissorsApp/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var cpuPoints = 0
+    @State var playerPoints = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("Points: 0")
+                .font(.system(size: 24))
+                .bold()
+            
+            Image("rock")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            
+            Image("rock")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            
+            Text("Points: 0")
+                .font(.system(size: 24))
+                .bold()
+            
+        }
     }
 }
 
